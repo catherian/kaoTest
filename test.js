64adf2eb614328ecf1b7ae6f9201955991f99839
catherian <znnxiaof@126.com>
2018-04-21 21:12:50 +0800

    ..

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
-const Koa = require('koa');
-var app = new Koa();
-app.listen(3000,()=>{
-    console.log("Server Started!");
-});
+// const Koa = require('koa');
+// var app = new Koa();
+// app.listen(3000,()=>{
+//     console.log("Server Started!");
+// });
+console.log("start!");
